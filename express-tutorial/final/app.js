cd537a6885913650577c29a33a6d7496e783e51c
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-11-18 16:37:05 +0100

    Add routes for home, about, and 404 pages

--- express-tutorial/final/app.js
+++ express-tutorial/final/app.js
@@ -5,6 +5,19 @@ app.listen(5000, () => {
   console.log("server is listening on port 5000....");
 });
 
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
 // app.get
 // app.post
 // app.put
