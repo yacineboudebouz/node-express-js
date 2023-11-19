d409367f8461fb624b29d274202bda3c07049cc9
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-11-19 21:57:08 +0100

    Add API routes for products and query

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,20 +1,45 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const { products } = require("./data");
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000 ✅");
 });
-// setup static and middleware
-app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+  res.send("<h1>Home Page</h1><a href='/api/products'>products</a>");
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+
+  res.json(newProducts);
 });
-app.get("/", (req, res) => {
-  res.sendFile();
+
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  // console.log(id);
+  const singleProduct = products.find((product) => product.id === Number(id));
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist");
+  }
+  res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  res.status(200).json(sortedProducts);
 });
