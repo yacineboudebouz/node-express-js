92fe38e2a964c144a72a693d6bf7ff9c4f57bfd1
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-11-20 18:02:13 +0100

    Add search functionality and limit feature to API endpoint

--- express-tutorial/final/7-query-params.js
+++ express-tutorial/final/7-query-params.js
@@ -2,14 +2,9 @@ const express = require("express");
 const app = express();
 const { products } = require("./data");
 
-app.listen(5000, () => {
-  console.log("server is listening on port 5000 ✅");
-});
-
 app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1><a href='/api/products'>products</a>");
+  res.send('<h1> Home Page</h1><a href="/api/products">products</a>');
 });
-
 app.get("/api/products", (req, res) => {
   const newProducts = products.map((product) => {
     const { id, name, image } = product;
@@ -18,18 +13,28 @@ app.get("/api/products", (req, res) => {
 
   res.json(newProducts);
 });
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req)
+  // console.log(req.params)
+  const { productID } = req.params;
 
-app.get("/api/products/:id", (req, res) => {
-  const { id } = req.params;
-  // console.log(id);
-  const singleProduct = products.find((product) => product.id === Number(id));
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
   if (!singleProduct) {
-    return res.status(404).send("Product does not exist");
+    return res.status(404).send("Product Does Not Exist");
   }
-  res.json(singleProduct);
+
+  return res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
 });
 
 app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query)
   const { search, limit } = req.query;
   let sortedProducts = [...products];
 
@@ -46,4 +51,8 @@ app.get("/api/v1/query", (req, res) => {
     return res.status(200).json({ sucess: true, data: [] });
   }
   res.status(200).json(sortedProducts);
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000....");
 });
