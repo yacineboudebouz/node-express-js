13480e2862351bd7231aed9fdc89a18a5f25f6cd
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-11-22 20:24:27 +0100

    middleware

--- express-tutorial/final/8-middleware-basic.js
+++ express-tutorial/final/8-middleware-basic.js
@@ -1,24 +1,18 @@
 const express = require("express");
 const app = express();
 const logger = require("./logger");
-const authorize = require("./authorize");
 
 // req => middleware => res
-app.use([authorize, logger]);
 
-app.get("/", (req, res) => {
-  res.send("Home");
+app.get("/", logger, (req, res) => {
+  res.json({
+    name: "John",
+    age: 30,
+  });
 });
 
 app.get("/about", (req, res) => {
   res.send("About");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("Products");
-});
-app.get("/api/items", (req, res) => {
-  res.send("Items");
 });
 
 app.listen(5000, () => {
